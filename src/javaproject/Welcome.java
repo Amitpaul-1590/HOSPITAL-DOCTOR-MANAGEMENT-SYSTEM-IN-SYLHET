10acd66ad67c35f3d68c5f62c375c9e5e3d04f6b
Amitpaul-1590 <amitpaul1590@gmail.com>
2021-10-26 23:06:44 +0600

    initial commit

--- src/javaproject/Welcome.java
+++ src/javaproject/Welcome.java
@@ -6,15 +6,16 @@ public class Welcome {
     Scanner input = new Scanner(System.in);
     String name;
     Welcome(){
-        System.out.println("Welcome to doctors info.");
+        System.out.println("Welcome to doctors info In Sylhet Sadar.");
         System.out.print("Please enter your name : ");
         name = input.nextLine();
 
     }
     void display(){
         System.out.println("\n");
-        System.out.println("\t\t\t\t\tDoctor's Info");
+        System.out.println("\t\t\t\t\tDoctor's Info In Sylhet Sadar");
         System.out.println("\t\t\t\t\t=============\n");
         System.out.println("Welcome,"+name+" to our system" );
+        System.out.println("Please select the category below");
     }
 }
